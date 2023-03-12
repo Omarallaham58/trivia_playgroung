a445092099720d43b3058cd2f1cb7771145b2866
Omarallaham58 <117765905+Omarallaham58@users.noreply.github.com>
2023-03-12 19:04:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 question_bank = []
-for i in question_data:
-    question_text = i["results"]["question"]
-    question_answer = i["results"]["correct_answer"]
+j=0
+while j<len(question_data[0]["results"]:
+  for i in question_data:
+    question_text = i["results"][j]["question"]
+    question_answer = i["results"][j]["correct_answer"]
     new_question = Question(question_text,question_answer)
     question_bank.append(new_question)
+     j+=1
 
 quiz = QuizBrain(question_bank)
 while quiz.still_has_question():
